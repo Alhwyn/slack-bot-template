64f75f5b49a3a5781020da7ff9f41b7077da9696
alhwyn <alhwyndwyne0411@gmail.com>
2024-08-07 17:06:32 -0700

    init commmit

---
+++ app.py
@@ -0,0 +1,95 @@
+from slack_bolt import App
+from slack_sdk.errors import SlackApiError
+from slack_bolt.adapter.flask import SlackRequestHandler
+from flask import Flask, request, jsonify
+import os
+import json
+from dotenv import load_dotenv, find_dotenv
+
+
+load_dotenv(find_dotenv())
+
+SLACK_SIGNING_SECRET = os.getenv('SLACK_SIGNING_SECRET')
+SLACK_BOT_TOKEN = os.getenv('SLACK_BOT_TOKEN')
+SLACK_BOT_USER_ID = os.getenv('SLACK_BOT_USER_ID')
+
+flask_app = Flask(__name__)
+app = App(token=SLACK_BOT_TOKEN, signing_secret=SLACK_SIGNING_SECRET)
+
+
+handler = SlackRequestHandler(app)
+
+
+# listen fur user mentoining the slack app
+@app.event("app_mention")
+def event_test(body, say, logger):
+    logger.info(body)
+    say("What's up?")
+
+@app.event("message")
+def handle_message_events(body, logger):
+    logger.info(body)
+
+@flask_app.route("/slack/events", methods=["POST"])
+def slack_events():
+    return handler.handle(request)
+
+
+
+@app.command("/command_example")
+def pricing_command(ack, say, body,  command, logger, client):
+    ack()
+    trigger_id = body['trigger_id']
+    try:
+        with open('command_example.json') as file:      
+            json_data = json.load(file)
+            say(json_data)
+    except Exception as e:
+        logger.error(f"Error Handling in /command_example {e}")
+
+
+@app.command("/modal_example")
+def pricing_command(ack, say, body,  command, logger, client):
+    ack()
+    trigger_id = body['trigger_id']
+    try:
+        with open('modal_example.json') as file:
+            json_data = json.load(file)
+            client.views_open(trigger_id=trigger_id, view=json_data)
+    except Exception as e:
+                logger.error(f"Error Handling in /modal_example {e}")
+
+@app.view("modal_example")
+def handle_pricing_submission(ack, body, logger):
+    ack()
+    try:
+        print('pricing modals works')
+    except Exception as e:
+        logger.error(f"Error Handling modal_example{e}")
+
+@app.command("/button_example")
+def pricing_command(ack, say, body,  command, logger, client):
+    ack()
+    trigger_id = body['trigger_id']
+    try:
+        with open('button_example.json') as file:      
+            json_data = json.load(file)
+            say(json_data)
+    except Exception as e:
+        logger.error(f"Error Handling /button_example {e}")
+
+@app.action("actionId-0")
+def pricing_command(ack, say, body, logger, client):
+    ack()
+    trigger_id = body['trigger_id']
+    try:
+        with open('example.json') as file:      
+            json_data = json.load(file)
+            say(trigger_id=trigger_id, blocks=json_data['blocks'])
+
+    except Exception as e:
+        logger.error(f"Error Handling hactionId-0 {e}")
+
+
+if __name__ == "__main__":
+    flask_app.run(host="0.0.0.0", port=5000)
